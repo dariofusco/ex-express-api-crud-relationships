221c2878e8588ebf3525e278472044cb20506bdf
dariofusco <fdario@hotmail.it>
2024-06-07 14:52:59 +0200

    aggiunge modelli category e tags con relazioni

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 
 const store = async (req, res, next) => {
 
-    const { title, slug, image, content, published, categoryId } = req.body;
+    const { title, slug, image, content, published, categoryId, tags } = req.body;
 
     const data = {
         title,
@@ -12,47 +12,103 @@ const store = async (req, res, next) => {
         content,
         published,
         categoryId,
+        tags: {
+            connect: tags.map(tag => ({ id: tag }))
+        }
     }
 
     try {
         const post = await prisma.post.create({ data });
         res.status(200).send(post);
-    } catch (next) {
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
     }
 }
 
 const index = async (req, res, next) => {
     try {
-        const posts = await prisma.post.findMany()
+        const posts = await prisma.post.findMany({
+            include: {
+                category: {
+                    select: {
+                        name: true,
+                    }
+                },
+                tags: {
+                    select: {
+                        name: true,
+                    }
+                }
+            }
+        });
         res.json(posts);
-    } catch (next) {
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
     }
 }
 
 const show = async (req, res, next) => {
     try {
         const { slug } = req.params;
-        const post = await prisma.post.findUnique({ where: { slug } });
-        res.json(post);
-    } catch (next) {
+
+        const post = await prisma.post.findUnique({
+            where: { slug },
+            include: {
+                category: {
+                    select: {
+                        name: true,
+                    }
+                },
+                tags: {
+                    select: {
+                        name: true,
+                    }
+                }
+            }
+        });
+        if (post) {
+            post.category = post.category.name;
+            post.tags = post.tags.map(tag => tag.name);
+            res.json(post);
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
     }
 }
 
 const update = async (req, res, next) => {
     try {
         const { slug } = req.params;
-        const post = await prisma.post.update({ where: { slug }, data: req.body });
+        const { title, image, content, published, categoryId, tags } = req.body;
+        const data = {
+            title,
+            image,
+            content,
+            published,
+            categoryId,
+            tags: {
+                set: tags.map(id => ({ id }))
+            }
+        }
+        const post = await prisma.post.update({ where: { slug }, data });
         res.json(post);
-    } catch (next) {
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
     }
 }
 
 const destroy = async (req, res, next) => {
-    const { slug } = req.params;
     try {
+        const { slug } = req.params;
         await prisma.post.delete({ where: { slug } });
         res.json(`Il post ${slug} è stato eliminato.`);
-    } catch (next) {
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
     }
 }
 
